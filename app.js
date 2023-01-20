596cb0f1b64df652ddbbd47fcad34f74cda48d17
AlanSquid <squid78313@gmail.com>
2023-01-20 12:47:47 +0800

    feat: add timer.js to start-stop the metronome

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import Timer from './timer.js'
+
 const tempoDisplay = document.querySelector('.tempo')
 const decreaseTempoBtn = document.querySelector('.decrease-tempo')
 const increaseTempoBtn = document.querySelector('.increase-tempo')
@@ -5,16 +7,20 @@ const tempoSlider = document.querySelector('.slider')
 const subtractBeats = document.querySelector('.subtract-beats')
 const addBeats = document.querySelector('.add-beats')
 const measureCount = document.querySelector('.measure-count')
+const startStopBtn = document.querySelector('.start-stop')
 
+const click1 = new Audio('click1.mp3')
+const click2 = new Audio('click2.mp3')
 
 let bpm = 140
 let beatsPerMeasure = 4
+let beatCount = 0
+let isRunning = false
 
 // 減號按鈕
 decreaseTempoBtn.addEventListener('click', () => {
   if (bpm <= 20) { return }
   bpm--
-  validateTempo()
   updateMetronome()
 })
 
@@ -22,14 +28,12 @@ decreaseTempoBtn.addEventListener('click', () => {
 increaseTempoBtn.addEventListener('click', () => {
   if (bpm >= 280) { return }
   bpm++
-  validateTempo()
   updateMetronome()
 })
 
 // 滑軌設定
 tempoSlider.addEventListener('input', () => {
   bpm = tempoSlider.value
-  validateTempo()
   updateMetronome()
 })
 
@@ -38,6 +42,7 @@ subtractBeats.addEventListener('click', () => {
   if (beatsPerMeasure <= 2) { return }
   beatsPerMeasure--
   measureCount.textContent = beatsPerMeasure
+  beatCount = 0
 })
 
 // 增加小節拍數
@@ -45,12 +50,43 @@ addBeats.addEventListener('click', () => {
   if (beatsPerMeasure >= 12) { return }
   beatsPerMeasure++
   measureCount.textContent = beatsPerMeasure
+  beatCount = 0
 })
 
+// start-stop
+startStopBtn.addEventListener('click', () => {
+  beatCount = 0
+  if (!isRunning) {
+    metronome.start()
+    isRunning = true
+    startStopBtn.textContent = 'STOP'
+  } else {
+    metronome.stop()
+    isRunning = false
+    startStopBtn.textContent = 'START'
+  }
+})
 
 
 // 更新BPM數值
 function updateMetronome() {
   tempoDisplay.textContent = bpm
   tempoSlider.value = bpm
+  metronome.timeInterval = 60000 / bpm
 }
+
+function playClick() {
+  if (beatCount === beatsPerMeasure) {
+    beatCount = 0
+  }
+  if (beatCount === 0) {
+    click1.play()
+    click1.currentTime = 0
+  } else {
+    click2.play()
+    click2.currentTime = 0
+  }
+  beatCount++
+}
+
+const metronome = new Timer(playClick, 60000 / bpm, { immediate: true })
